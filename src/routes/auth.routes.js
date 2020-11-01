e5ed18a1007b7eb36e09d7126ae9fe98a486d7b4
kallebysantos <kalleby.santos2k@gmail.com>
2020-11-01 22:22:54 +0000

    feat: auth schema

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,15 @@
-const { auth } = require("../server");
-
 const authHandler = require('../handlers/auth.handler');
+const authSchema = require('../schemas/auth.schema');
 
 module.exports = [
   {
     method: 'POST',
     path: '/login',
     handler: authHandler.login,
+    options: {
+      validate: {
+        payload: authSchema,
+      },
+    },
   },
 ];
