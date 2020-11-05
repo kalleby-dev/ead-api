241a4f21757c3ab16b34db4a263755d4195030a7
kallebysantos <kalleby.santos2k@gmail.com>
2020-11-05 09:13:10 +0000

    feat: Del. user cache

--- src/repositories/users.repository.js
+++ src/repositories/users.repository.js
@@ -31,8 +31,11 @@ const setCache = (user) => {
   cache.set(cache.PREFIX.user(user.id), cache.toCache(user), LOGIN_EXP_TIME);
 };
 
+const delCache = (userId) => cache.del(cache.PREFIX.user(userId));
+
 module.exports = {
   create,
   findByEmail,
   setCache,
+  delCache,
 };
